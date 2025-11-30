78fb06873b8b00b043e2c5620d1699f3e150e580
Arkadiusz Chmura <arekchmura@gmail.com>
2025-11-30 19:02:12 +0100

    Add a template

---
+++ src/main/kotlin/com/clouddjr/advent2025/Resources.kt
@@ -0,0 +1,24 @@
+package com.clouddjr.advent2025
+
+import java.io.File
+import java.net.URI
+
+object Resources {
+    fun resourceAsText(fileName: String): String =
+        File(fileName.toURI()).readText()
+
+    fun resourceAsListOfString(fileName: String): List<String> =
+        File(fileName.toURI()).readLines()
+
+    fun resourceAsListOfInt(fileName: String): List<Int> =
+        resourceAsListOfString(fileName).map { it.toInt() }
+
+    fun resourceAsListOfLong(fileName: String): List<Long> =
+        resourceAsListOfString(fileName).map { it.toLong() }
+
+    fun resourceAsString(fileName: String, delimiter: String = ""): String =
+        resourceAsListOfString(fileName).reduce { a, b -> "$a$delimiter$b" }
+
+    private fun String.toURI(): URI =
+        Resources.javaClass.classLoader.getResource(this)?.toURI() ?: error("Cannot find resource: $this")
+}
